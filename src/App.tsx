bee358aa32a6e1698825e68451bbf63800ae837b
aidankahng <aidankahng@gmail.com>
2024-04-15 12:30:47 -0700

    started testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // return React.createElement('h1', {}, 'This is a title');
+    //Without using JSX
+    // return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'), React.createElement('h2', {}, 'This is some useless text'))
+    // Using JSX below
+    // return (
+    //   <div>
+    //     <h1>Hello There</h1>
+    //     <h2>This is some useless text</h2>
+    //   </div>
+    // )
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Hook (setup using useState)
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+    const toggleLogin = () => {
+        setIsLoggedIn(!isLoggedIn);
+    }
+
+
+    const posts = [
+        { id: 1, title: 'Happy'},
+        { id: 2, title: 'Meh'},
+        { id: 3, title: 'Sad'},
+    ]
+
+
+
+    const firstName:string = 'Johnathan';
+    
+    return (
+        <div>
+            <h2>
+                The name is: {firstName.toUpperCase()}
+            </h2>
+            <p>{isLoggedIn ? 'Welcome Back' : 'Please Log In'}</p>
+            <button onClick={toggleLogin}>LOGIN BTN</button>
+            <br />
+            <input type="text" name="TestingInput" id="" placeholder="input random shit here" autoFocus/>
+            {posts.map((post) => <p>{`${post.id} -- ${post.title}`}</p> )}
+        </div>
+    )
+
+
+
 }
 
-export default App
+export default App;
